9dfb898c40b7420b1ac52652eb22c1bc7a2ee553
dalhaqq <63442570+dalhaqq@users.noreply.github.com>
2021-03-22 00:01:43 +0700

    Update table.py

--- table.py
+++ table.py
@@ -4,7 +4,7 @@ def show_table(header,datas,format=None):
   cols = len(header)
   header = "|" + "|".join(header) + "|"
   if format == None:
-    format = "|" + "|".join(["---" for col in range(cols)]) + "|"
+    format = "|" + "|".join(["---" for col in range(0,cols)]) + "|"
   table = header
   table += "\n"
   table += format
