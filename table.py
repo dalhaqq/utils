8a074248e5aa041ac3d4daa09f3670e726a11570
dalhaqq <63442570+dalhaqq@users.noreply.github.com>
2021-03-21 23:57:56 +0700

    Create table.py

---
+++ table.py
@@ -0,0 +1,15 @@
+from IPython.display import Markdown as md
+
+def show_table(header,datas,format=None):
+  cols = len(header)
+  header = "|" + "|".join(header) + "|"
+  if format == None:
+    format = "|" + "|".join(["---" for col in cols]) + "|"
+  table = header
+  table += "\n"
+  table += format
+  table += "\n"
+  for data in datas:
+    table += "|" + "|".join(data) + "|"
+    table += "\n"
+  md(table)
